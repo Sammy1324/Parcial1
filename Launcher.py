4926d5fbb7d35d23aa2cd40bb9e1eca7c264f741
Sammy1234 <sammyav2004@gmail.com>
2025-04-01 09:45:40 +0200

    Launcher convertido a clase

--- Launcher.py
+++ Launcher.py
@@ -6,53 +6,54 @@ B = Point(5, 5)
 C = Point(-3, -1)
 D = Point(0, 0)
 
-def show_points():
+class Launcher:
+    def show_points():
 
-    print("Puntos:")
-    print(f"A: {A}")
-    print(f"B: {B}")
-    print(f"C: {C}")
-    print(f"D: {D}")
+        print("Puntos:")
+        print(f"A: {A}")
+        print(f"B: {B}")
+        print(f"C: {C}")
+        print(f"D: {D}")
 
-def show_quadrants(A=A, C=C, D=D):
-    print("\nCuadrantes:")
-    print(f"A pertenece al cuadrante: {A.quadrant()}")
-    print(f"C pertenece al cuadrante: {C.quadrant()}")
-    print(f"D pertenece al cuadrante: {D.quadrant()}")
+    def show_quadrants(A=A, C=C, D=D):
+        print("\nCuadrantes:")
+        print(f"A pertenece al cuadrante: {A.quadrant()}")
+        print(f"C pertenece al cuadrante: {C.quadrant()}")
+        print(f"D pertenece al cuadrante: {D.quadrant()}")
 
-def show_vectors(A=A, B=B):
-    print("\nVectores:")
-    print(f"Vector AB: {A.vector(B)}")
-    print(f"Vector BA: {B.vector(A)}")
+    def show_vectors(A=A, B=B):
+        print("\nVectores:")
+        print(f"Vector AB: {A.vector(B)}")
+        print(f"Vector BA: {B.vector(A)}")
 
-def show_distances(A=A, B=B, C=C, D=D):
-    print("\nDistancias:")
-    print(f"Distancia entre A y B: {A.distance(B)}")
-    print(f"Distancia entre B y A: {B.distance(A)}")
+    def show_distances(A=A, B=B, C=C, D=D):
+        print("\nDistancias:")
+        print(f"Distancia entre A y B: {A.distance(B)}")
+        print(f"Distancia entre B y A: {B.distance(A)}")
 
-    distance_A = A.distance(D)
-    distance_B = B.distance(D)
-    distance_C = C.distance(D)
+        distance_A = A.distance(D)
+        distance_B = B.distance(D)
+        distance_C = C.distance(D)
 
-    farther = max(distance_A, distance_B, distance_C)
-    if farther == distance_A:
-        print("\nEl punto más lejos del origen es A.")
-    elif farther == distance_B:
-        print("\nEl punto más lejos del origen es B.")
-    else:
-        print("\nEl punto más lejos del origen es C.")
+        farther = max(distance_A, distance_B, distance_C)
+        if farther == distance_A:
+            print("\nEl punto más lejos del origen es A.")
+        elif farther == distance_B:
+            print("\nEl punto más lejos del origen es B.")
+        else:
+            print("\nEl punto más lejos del origen es C.")
 
-def show_rectangle(A=A, B=B):
-    rectangle = Rectangle(A, B)
+    def show_rectangle(A=A, B=B):
+        rectangle = Rectangle(A, B)
 
-    print("\nRectángulo:")
-    print(f"Base: {rectangle.basis()}")
-    print(f"Altura: {rectangle.height()}")
-    print(f"Área: {rectangle.area()}")
+        print("\nRectángulo:")
+        print(f"Base: {rectangle.basis()}")
+        print(f"Altura: {rectangle.height()}")
+        print(f"Área: {rectangle.area()}")
 
 def main():
-    show_points()
-    show_quadrants(A, C, D)
-    show_vectors(A, B)
-    show_distances(A, B, C, D)
-    show_rectangle(A, B)
+    Launcher.show_points()
+    Launcher.show_quadrants(A, C, D)
+    Launcher.show_vectors(A, B)
+    Launcher.show_distances(A, B, C, D)
+    Launcher.show_rectangle(A, B)
